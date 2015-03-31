798d30767ff3c56ba27e3073fb37576c081a5d27
Hugo Marcotte <marcotte.hugo@gmail.com>
2015-03-31 16:23:57 -0400

    Fixing delays in answering questions and potentially fixed some problems with iPhone hanging after answering a question.

--- client/app/question/question.controller.js
+++ client/app/question/question.controller.js
@@ -2,12 +2,15 @@
 
 angular.module('fuirApp')
   .controller('QuestionCtrl', function ($scope, Question) {
-
+    var questionAnswered = false;
+    $scope.showResult = false;
 
     $scope.question = $scope.ngDialogData.question;
     $scope.randomGuyNumber = $scope.ngDialogData.randomGuyNumber;
     $scope.cardColor = $scope.ngDialogData.cardColor;
 
+
+    console.log($scope.question);
 
     $scope.getRandomGuy = function(num, color) {
 
@@ -27,57 +30,68 @@ angular.module('fuirApp')
       return false;
     };
 
-    var saving = false;
+
     $scope.saveAnswer = function(answerIndex) {
 
-      // console.log('saving');
-      // Question.getQuestion($scope.question.objectId)
-      // .then(function(question){
-      //
-      //   // Add results to question in scope
-      //   $scope.question.results = {
-      //     inMajority: false,
-      //     percentAnsw1: 60,
-      //     percentAnsw2: 40,
-      //     totalAnswerCount: 400
-      //   };
-      //
-      //   $scope.question.answered = true;
-      //
-      // })
-      // .catch(function(err){
-      //   console.log(err);
-      // });
+      if(!questionAnswered) {
+        questionAnswered = true;
 
-      if(!saving) {
-        saving = true;
+
+        // Question.getQuestion($scope.question.objectId)
+        // .then(function(question){
+        //
+        //   // Add results to question in scope
+        //   $scope.results = {
+        //     inMajority: false,
+        //     percentAnsw1: 100,
+        //     percentAnsw2: 0,
+        //     totalAnswerCount: 500
+        //   };
+        //
+        // })
+        // .catch(function(err){
+        //   console.log(err);
+        // });
+
+        // To avoid delays in showing the result, calculate result on the front end and then save answer to Db
+        $scope.results = {};
+        $scope.question.totalAnswerCount += 1;
+
+        if(answerIndex === 0) {
+          $scope.question.countAnswer1 += 1;
+
+          if($scope.question.countAnswer1 >= $scope.question.countAnswer2) {
+            $scope.results.inMajority = true;
+          }
+          else {
+            $scope.results.inMajority = false;
+          }
+        }
+        else {
+          $scope.question.countAnswer2 += 1;
+
+          if($scope.question.countAnswer2 >= $scope.question.countAnswer1) {
+            $scope.results.inMajority = true;
+          }
+          else {
+            $scope.results.inMajority = false;
+          }
+        }
+
+        // Add results to question in scope
+        $scope.results.percentAnsw1 = Math.ceil(($scope.question.countAnswer1 / $scope.question.totalAnswerCount) * 100);
+        $scope.results.percentAnsw2 = Math.floor(($scope.question.countAnswer2 / $scope.question.totalAnswerCount) * 100);
+        $scope.results.totalAnswerCount = $scope.question.totalAnswerCount;
+
+        $scope.showResult = true;
 
         // Save answer
         Question.saveAnswer($scope.question.objectId, answerIndex)
         .success(function(newAnswer) {
-
-          // Get question with new count values
-          Question.getQuestion($scope.question.objectId)
-          .success(function(question) {
-
-            // Add results to question in scope
-            $scope.question.results = {
-                  inMajority: newAnswer.inMajority,
-                  percentAnsw1: Math.ceil((question.countAnswer1 / question.totalAnswerCount) * 100),
-                  percentAnsw2: Math.floor((question.countAnswer2 / question.totalAnswerCount) * 100),
-                  totalAnswerCount: question.totalAnswerCount
-            };
-
-            $scope.question.answered = true;
-          })
-          .error(function(err) {
-            console.log('Cannot get results');
-            console.log(err);
-          });
-
+          console.log('Answer has been saved!')
         })
         .error(function(err) {
-          console.log('Cannot save answer');
+          console.log('Error: could\'nt save answer');
           console.log(err);
         });
 
